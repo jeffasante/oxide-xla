ca697d26441861b80f9d7d9b428c2d5f533601b8
Jeffrey Asante <jeff@Jeffreys-MacBook-Pro.local>
2026-03-10 21:13:39 +0000

    fix: include WASM package by removing the auto-generated .gitignore from pkg/

---
+++ docs/pkg/oxide_xla_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_compilationresult_free: (a: number, b: number) => void;
+export const compilationresult_code: (a: number) => [number, number];
+export const compilationresult_json: (a: number) => [number, number];
+export const transpile_onnx: (a: number, b: number) => [number, number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
